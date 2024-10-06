c994c76a34f8b70b12ea31cf7ea24f3674503fdb
eunhyun05 <eunhyun050812@gmail.com>
2024-10-06 23:11:21 +0900

    feat: validate_security_key 함수로 변경

--- src/store/handler.rs
+++ src/store/handler.rs
@@ -1,18 +1,13 @@
 use std::sync::Arc;
 use axum::{Extension, Json, Router};
-use axum::http::StatusCode;
+use axum::http::{HeaderMap, StatusCode};
 use axum::response::IntoResponse;
 use axum::routing::post;
-use axum_extra::headers::Authorization;
-use axum_extra::headers::authorization::Bearer;
-use axum_extra::TypedHeader;
-use bson::oid::ObjectId;
-use crate::common::jwt::validate_jwt;
 use crate::common::response::ErrorResponse;
 use crate::common::types::Status;
 use crate::database::MongoRepository;
 use crate::store::model::{CreateStoreRequest, CreateStoreResponse, Store};
-use crate::user::model::Rank;
+use crate::user::helper::validate_security_key;
 
 pub fn store_routes() -> Router {
     Router::new()
@@ -20,50 +15,12 @@ pub fn store_routes() -> Router {
 }
 
 pub async fn create_store(
-    TypedHeader(Authorization(bearer)): TypedHeader<Authorization<Bearer>>,
+    headers: HeaderMap,
     Extension(mongo_repo): Extension<Arc<MongoRepository>>,
     Json(body): Json<CreateStoreRequest>,
 ) -> Result<impl IntoResponse, StatusCode> {
-    let token = bearer.token();
-    let claims = match validate_jwt(token) {
-        Ok(claims) => claims.claims,
-        Err(_) => {
-            let error_response = ErrorResponse {
-                status: Status::Error,
-                message: "유효하지 않은 토큰입니다.".to_string(),
-            };
-            return Ok((StatusCode::UNAUTHORIZED, Json(error_response)).into_response());
-        }
-    };
-
-    let user_id = match claims.id.parse::<ObjectId>() {
-        Ok(id) => id,
-        Err(_) => {
-            let error_response = ErrorResponse {
-                status: Status::Error,
-                message: "유효하지 않은 사용자 ID입니다.".to_string(),
-            };
-            return Ok((StatusCode::UNAUTHORIZED, Json(error_response)).into_response());
-        }
-    };
-
-    let user = match mongo_repo.find_user_by_id(&*body.name, &user_id).await {
-        Some(user) => user,
-        None => {
-            let error_response = ErrorResponse {
-                status: Status::Error,
-                message: "유저를 찾을 수 없습니다.".to_string(),
-            };
-            return Ok((StatusCode::UNAUTHORIZED, Json(error_response)).into_response());
-        }
-    };
-
-    if user.rank != Rank::Administrator {
-        let error_response = ErrorResponse {
-            status: Status::Failure,
-            message: "관리자 권한이 없습니다.".to_string(),
-        };
-        return Ok((StatusCode::FORBIDDEN, Json(error_response)).into_response());
+    if let Err(err) = validate_security_key(&headers) {
+        return Ok(err.into_response());
     }
 
     if let Some(_) = mongo_repo.find_store_by_name(&body.name).await {
