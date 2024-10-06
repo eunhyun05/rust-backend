97be3d3301717a7db554966116f93115509bd1b4
eunhyun05 <eunhyun050812@gmail.com>
2024-10-06 18:32:57 +0900

    feat: to -> to_list로 보내지도록 변경

--- src/common/email.rs
+++ src/common/email.rs
@@ -2,13 +2,12 @@ use resend_rs::types::CreateEmailBaseOptions;
 use resend_rs::{Resend, Result};
 use crate::config::CONFIG;
 
-pub async fn send_email(to: &str, subject: &str, body: &str) -> Result<()> {
+pub async fn send_email(to: &[str], subject: &str, body: &str) -> Result<()> {
     let resend = Resend::new(&*CONFIG.resend_api_key);
 
     let from = "Acme <onboarding@resend.dev>";
-    let to_list = [to];
 
-    let email = CreateEmailBaseOptions::new(from, to_list, subject)
+    let email = CreateEmailBaseOptions::new(from, to, subject)
         .with_html(body);
 
     let _email = resend.emails.send(email).await?;
