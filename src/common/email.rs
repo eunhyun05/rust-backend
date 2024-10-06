4a88ad27399050ae4d8d9663076fcef7210026a5
eunhyun05 <eunhyun050812@gmail.com>
2024-10-06 21:19:42 +0900

    feat: &[str] -> Vec<String>로 타입 수정

--- src/common/email.rs
+++ src/common/email.rs
@@ -2,7 +2,8 @@ use resend_rs::types::CreateEmailBaseOptions;
 use resend_rs::{Resend, Result};
 use crate::config::CONFIG;
 
-pub async fn send_email(to: &[str], subject: &str, body: &str) -> Result<()> {
+#[allow(dead_code)]
+pub async fn send_email(to: Vec<String>, subject: &str, body: &str) -> Result<()> {
     let resend = Resend::new(&*CONFIG.resend_api_key);
 
     let from = "Acme <onboarding@resend.dev>";
